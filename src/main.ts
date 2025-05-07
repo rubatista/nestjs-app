740468b2827c070ac6c24e7984bd92767927ceb3
rubatista <rubatista.dev@gmail.com>
2025-05-07 10:58:29 +0100

    fix(main.ts): redine port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // Atualiza para a porta do teu frontend NuxtJS
+    credentials: true, // Se estiveres a usar cookies ou autenticação com credenciais
+  });
+
+  await app.listen(3000); // backend will runn at port 3000
 }
 bootstrap();
